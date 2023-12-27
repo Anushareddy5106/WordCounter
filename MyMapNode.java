41d561b5a8b954e3d91cd10453ab1a4b1cbe48ec
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-27 19:25:10 +0530

    MyMapNode created

---
+++ MyMapNode.java
@@ -0,0 +1,11 @@
+public class MyMapNode {
+    public String key;
+    public int value;
+    public MyMapNode next;
+
+    MyMapNode(String key) {
+        this.key = key;
+        this.value = 1;
+        this.next = null;
+    }
+}
